8cb36e19670c1541756c52794600af50f684604a
Prasanna <prasanmouli@gmail.com>
2013-08-09 01:16:00 +0530

    additions to zh object

--- scripts/zh.js
+++ scripts/zh.js
@@ -1,7 +1,6 @@
 var canvas = document.getElementById('levels');
 var context = canvas.getContext('2d');
 
-var pix = 5;
 var requestId;
 var land = [{
 	"width" : 800,
@@ -25,15 +24,17 @@ var land = [{
 	}];
 
 
-var ZH = {
-	"X" : 50,
-	"Y" : 485 - land[0].height,
-	"jump" : {
+function ZombieHunter() {
+	this.X = 50;
+	this.Y = 485-land[0].height;
+	this.jump = {
 		"ascent" : false,
 		"descent" : false 
-		},
-	"velocity" : 9.87
-	};
+		};
+	this.velocity = 8.00;
+	this.pix = 5;
+	this.bullets = 20;
+}
 
 var num = 1;
 var interv = {};	
@@ -41,15 +42,18 @@ var interv = {};
 document.body.addEventListener("keydown", function(e){
 	
 	if(e.keyCode == 67 && ZH.jump.ascent == false && ZH.jump.descent == false){
+	  	console.log("adas");
 	  	playerAscent();
 	}
 	
 	if(e.keyCode == 39){
-		shoot(num++, "normal");
+		spillBullet = new shoot(1);// 1:"normal"
+		spillBullet.interv();
 	}
 	
-	if(e.keyCode==88){
-		shoot(num++, "cannon")
+	if(e.keyCode == 88){
+		spillCannon = new shoot(2);// 2: "cannon"
+		spillCannon.interv();
 	}
 	
 }, true);	
@@ -73,73 +77,14 @@ window.cancelAnimFrame = (function(){
 var canvs2 = document.getElementById('levels');
 var canvs3 = document.getElementById('player');
 
-function levels(){
-
-	requestId = requestAnimFrame(levels);
-	if(ZH.jump.descent==false && ZH.jump.ascent==false && (ZH.X+land[0].pitGap-land[1].pos)<=10 && ZH.X>land[0].appWidth){
-		cancelAnimFrame(requestId);
-		requestId = undefined;
-		alert('You lost!');
-	}
-	
-	for(i=0; i<2; i++){
-				
-		if(land[i].freedom == false){
-			context.clearRect(land[i].width-(land[i].Yi)%(land[i].width+land[i].pitGap), 500-land[i].height-1, pix+2, land[i].height+2 );
-			context.beginPath();
-			context.rect(0, 500-land[i].height, land[i].width-(land[i].Yi)%(land[i].width+land[i].pitGap), land[i].height);
-			context.fillStyle = 'yellow';
-			context.fill();
-			context.lineWidth = 2;
-			context.strokeStyle = 'black';
-			context.stroke();
-			land[i].Yi += pix;
-			land[i].appWidth -= pix;
-			land[i].pos = 0;
-			if((land[i].Yi)%(land[i].width+land[i].pitGap)==0 && land[i].Yi>0)
-				context.clearRect(0, 500-land[i].height-1, land[i].width-(land[i].Yi)%(land[i].width+land[i].pitGap)+1, land[i].height+2);
-		}
-		else{
-			land[i].pos = land[i-1].appWidth+land[i-1].pitGap;
-			context.beginPath();
-			if(land[i].height > land[i-1].height){
-				context.clearRect(land[i-1].width+land[i-1].pitGap-(land[i].Xi)%(land[i-1].width+land[i-1].pitGap), 500-land[i].height-1, land[i].width, land[i].height+2);
-				context.clearRect(land[i-1].width+land[i-1].pitGap+land[i].width-(land[i].Xi)%(land[i-1].width+land[i-1].pitGap), 500-land[i].height-1, pix+2, land[i].height+2);
-			}
-			else{
-				context.clearRect(land[i-1].width+land[i-1].pitGap-(land[i].Xi)%(land[i-1].width+land[i-1].pitGap), 500-land[i-1].height-1, land[i].width, land[i-1].height+2);
-				context.clearRect(land[i-1].width+land[i-1].pitGap+land[i].width-(land[i].Xi)%(land[i-1].width+land[i-1].pitGap), 500-land[i-1].height-1, pix+2, land[i-1].height+2);
-			}
-			context.rect(land[i-1].width+land[i-1].pitGap-(land[i].Xi)%(land[i-1].width+land[i-1].pitGap), 500-land[i].height, land[i].width, land[i].height);
-			if(land[i-1].width+land[i-1].pitGap-land[i].Xi <=5 && land[i].Xi>0){
-				context.clearRect(0, 500-land[i].height-1, land[i].width+2, land[i].height+2);
-				land[i].freedom = false;
-				land[i].appWidth = land[i].width;
-				w = Math.floor(Math.random()*40)+1000+pix*25;
-				h = Math.floor(Math.random()*100)+170;
-				pitGap = Math.floor(Math.random()*10+200+pix*6);
-				pos = land[i-1].width+land[i-1].pitGap-land[i].Xi+land[i].width+land[i].pitGap;
-				land.push({"width" : w,"height" : h,"pitGap" : pitGap,"Xi" : 0,"Yi" : 0,"appWidth" : w,"pos" : pos,"freedom" : true});
-				land.splice(i-1, 1);
-				pix +=0.1;
-			}
-			context.fill();
-			context.lineWidth = 2;
-			context.strokeStyle = 'black';
-			context.stroke();
-			land[i].Xi += pix;
-		}
-	}
-	
-}
 
 function playerAscent(){
-	
 	ZH.jump.ascent = true;
 	var time=0.0, v=0.0;
 	
     var inter = setInterval(function(){	
 		if(v<0){
+			console.log(ZH.Y);
 			clearInterval(inter);
 			ZH.jump.ascent = false;
 			playerDescent();
@@ -164,8 +109,8 @@ function playerDescent(){
 	
     var inter = setInterval(function(){
 		
-		t0 = (land[0].pos-ZH.X)/(pix*60);
-		t1 = (land[1].pos-ZH.X)/(pix*60);
+		t0 = (land[0].pos-ZH.X)/(ZH.pix*60);
+		t1 = (land[1].pos-ZH.X)/(ZH.pix*60);
 		
 		if(t0<time)
 			expr = ZH.Y + land[0].height;
@@ -194,40 +139,110 @@ function playerDescent(){
 	
 }
 
-
-function shoot(i, k){
+function shoot(k){
 	var x=0;
 	var Y = ZH.Y;
-	interv[i] = setInterval(function(){
-		var bullet = canvs3.getContext("2d");
-		if(k==="normal"){
+	
+	this.interv = function(){
+	var bullet = canvs3.getContext("2d");
+	var g = setInterval(function(){
+	  if(k==1){	
+		bullet.beginPath();  
+		bullet.clearRect(ZH.X+x+10, Y+2, 4, 6);
+		x+=3+Math.floor(ZH.pix/25);
+		bullet.fillRect(ZH.X+x+10, Y+3, 4, 4);
+		console.log(Y+" "+land[0].height+" "+land[1].height);
+		if(ZH.X+x > 1000 || (ZH.X+x>=land[1].pos && Y+9>(500-land[1].height))){
+			clearInterval(g);
 			bullet.clearRect(ZH.X+x+10, Y+2, 4, 6);
-			x+=3+Math.floor(pix/25);
-			bullet.fillRect(ZH.X+x+10, Y+3, 4, 4);
-			console.log(Y+" "+land[0].height+" "+land[1].height);
-			if(ZH.X+x > 1000 || (ZH.X+x>=land[1].pos && (land[1].height-land[0].height)>12 && Y+9>(500-land[1].height))){
-				clearInterval(interv[i]);
-				bullet.clearRect(ZH.X+x+10, Y+2, 4, 6);
 			}
+	  }
+	  else{
+		console.log("asdasd");
+		bullet.clearRect(ZH.X-x+21, Y+1, 8, 8);
+		x+=3+Math.floor(ZH.pix/25);
+		bullet.arc(ZH.X+x+25, Y+5, 3, 0, 2*Math.PI);
+//		console.log(Y+" "+land[0].height+" "+land[1].height);
+		if(ZH.X+x > 1000 || (ZH.X+x>=land[1].pos && Y+9>(500-land[1].height))){
+			clearInterval(g);
+			bullet.clearRect(ZH.X+x+21, Y+1, 8, 8);
+			}
+	  }
+    }, 10);
+	}
+}
+
+function levels(){
+	
+	requestId = requestAnimFrame(levels);
+	if(ZH.jump.descent==false && ZH.jump.ascent==false && (ZH.X+land[0].pitGap-land[1].pos)<=10 && ZH.X>land[0].appWidth){
+		cancelAnimFrame(requestId);
+		requestId = undefined;
+		alert('You lost!');
+	}
+	
+	for(i=0; i<2; i++){
+				
+		if(land[i].freedom == false){
+			context.clearRect(land[i].width-(land[i].Yi)%(land[i].width+land[i].pitGap), 500-land[i].height-1, ZH.pix+2, land[i].height+2 );
+			context.beginPath();
+			context.rect(0, 500-land[i].height, land[i].width-(land[i].Yi)%(land[i].width+land[i].pitGap), land[i].height);
+			context.fillStyle = 'yellow';
+			context.fill();
+			context.lineWidth = 2;
+			context.strokeStyle = 'black';
+			context.stroke();
+			land[i].Yi += ZH.pix;
+			land[i].appWidth -= ZH.pix;
+			land[i].pos = 0;
+			if((land[i].Yi)%(land[i].width+land[i].pitGap)==0 && land[i].Yi>0)
+				context.clearRect(0, 500-land[i].height-1, land[i].width-(land[i].Yi)%(land[i].width+land[i].pitGap)+1, land[i].height+2);
 		}
 		else{
-			bullet.clearRect(ZH.X+x+20, Y+1, 6, 8);
-			x+=3+Math.floor(pix/25);
-			bullet.fillRect(ZH.X+x+20, Y+2, 6, 6);
-			console.log(Y+" "+land[0].height+" "+land[1].height);
-			if(ZH.X+x > 1000 || (ZH.X+x>=land[1].pos && (land[1].height-land[0].height)>12 && Y+9>(500-land[1].height))){
-				clearInterval(interv[i]);
-				bullet.clearRect(ZH.X+x+20, Y+1, 6, 8);
-				}
+			land[i].pos = land[i-1].appWidth+land[i-1].pitGap;
+			context.beginPath();
+			if(land[i].height > land[i-1].height){
+				context.clearRect(land[i-1].width+land[i-1].pitGap-(land[i].Xi)%(land[i-1].width+land[i-1].pitGap), 500-land[i].height-1, land[i].width, land[i].height+2);
+				context.clearRect(land[i-1].width+land[i-1].pitGap+land[i].width-(land[i].Xi)%(land[i-1].width+land[i-1].pitGap), 500-land[i].height-1, ZH.pix+2, land[i].height+2);
 			}
-	}, 10);
+			else{
+				context.clearRect(land[i-1].width+land[i-1].pitGap-(land[i].Xi)%(land[i-1].width+land[i-1].pitGap), 500-land[i-1].height-1, land[i].width, land[i-1].height+2);
+				context.clearRect(land[i-1].width+land[i-1].pitGap+land[i].width-(land[i].Xi)%(land[i-1].width+land[i-1].pitGap), 500-land[i-1].height-1, ZH.pix+2, land[i-1].height+2);
+			}
+			context.rect(land[i-1].width+land[i-1].pitGap-(land[i].Xi)%(land[i-1].width+land[i-1].pitGap), 500-land[i].height, land[i].width, land[i].height);
+			if(land[i-1].width+land[i-1].pitGap-land[i].Xi <=5 && land[i].Xi>0){
+				context.clearRect(0, 500-land[i].height-1, land[i].width+2, land[i].height+2);
+				land[i].freedom = false;
+				land[i].appWidth = land[i].width;
+				var w = Math.floor(Math.random()*40)+1000+ZH.pix*25;
+				while(1){
+					var h = Math.floor(Math.random()*100)+170;
+					if(h-land[i].height<60) break;
+				}
+				var pitGap = Math.floor(Math.random()*10+200+ZH.pix*6);
+				var pos = land[i-1].width+land[i-1].pitGap-land[i].Xi+land[i].width+land[i].pitGap;
+				land.push({"width" : w,"height" : h,"pitGap" : pitGap,"Xi" : 0,"Yi" : 0,"appWidth" : w,"pos" : pos,"freedom" : true});
+				land.splice(i-1, 1);
+				ZH.pix +=0.1;
+			}
+			context.fill();
+			context.lineWidth = 2;
+			context.strokeStyle = 'black';
+			context.stroke();
+			land[i].Xi += ZH.pix;
+		}
+	}
+	
 }
 
 window.onload = function(){
+
+	ZH = new ZombieHunter();
 	c.fillStyle = "#FF0000";	
 	c.fillRect(ZH.X, ZH.Y, 10, 10);
     c.fill();
     	
 	levels();
+	
 	
 };
